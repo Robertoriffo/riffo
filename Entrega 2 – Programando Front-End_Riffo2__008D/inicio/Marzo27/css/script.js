f387672b3c1691a42196e4f954ba671306513c80
Robertoriffo <134812383+Robertoriffo@users.noreply.github.com>
2024-05-14 11:11:05 -0400

    Delete Entrega 2 – Programando Front-End_Riffo2__008D directory

--- Entrega 2 – Programando Front-End_Riffo2__008D/inicio/Marzo27/css/script.js
+++
@@ -1,7 +0,0 @@
-function mostrarDetalles(nombre, precio) {
-    var nombreProducto = document.getElementById('nombre');
-    var precioProducto = document.getElementById('precio');
-
-    nombreProducto.textContent = "Nombre: " + nombre;
-    precioProducto.textContent = "Precio: $" + precio;
-}
